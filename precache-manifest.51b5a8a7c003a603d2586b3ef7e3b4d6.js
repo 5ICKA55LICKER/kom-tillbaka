2a8178ad531a6d50601428523eb95573c41fb709
blixt <teodor.blixt@gmail.com>
2020-05-18 21:13:09 +0200

    Updates

---
+++ precache-manifest.51b5a8a7c003a603d2586b3ef7e3b4d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcdb8612009cbbf1b32f12687e65a401",
+    "url": "/5ICKA55LICKER/kom-tillbaka.github.io/index.html"
+  },
+  {
+    "revision": "0ee0871437372a6bfe85",
+    "url": "/5ICKA55LICKER/kom-tillbaka.github.io/static/css/main.b9238fa7.chunk.css"
+  },
+  {
+    "revision": "ac50f66e633a4490c4ad",
+    "url": "/5ICKA55LICKER/kom-tillbaka.github.io/static/js/2.5aa14809.chunk.js"
+  },
+  {
+    "revision": "296362f3435e3c4e1832bddadf6a9199",
+    "url": "/5ICKA55LICKER/kom-tillbaka.github.io/static/js/2.5aa14809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ee0871437372a6bfe85",
+    "url": "/5ICKA55LICKER/kom-tillbaka.github.io/static/js/main.a8a8c1c8.chunk.js"
+  },
+  {
+    "revision": "26dbcb56fbaf1f6ebee1",
+    "url": "/5ICKA55LICKER/kom-tillbaka.github.io/static/js/runtime-main.f2d0bc00.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kom-tillbaka.github.io/precache-manifest.c4bddea6cf9158ef0af4c1b4550583b4.js"
+  "/5ICKA55LICKER/kom-tillbaka.github.io/precache-manifest.51b5a8a7c003a603d2586b3ef7e3b4d6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kom-tillbaka.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/5ICKA55LICKER/kom-tillbaka.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
